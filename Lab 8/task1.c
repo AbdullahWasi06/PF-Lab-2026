4109e02ed14b8707f28f9cbf7323d8fa4ac880fb
AbdullahWasi06 <k256502@nu.edu.pk>
2026-03-13 20:33:27 +0500

    Update task1.c

--- Lab 8/task1.c
+++ Lab 8/task1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-	int sum =0;
+	int sum =0, subsum = 0, j;
 	int array[4][3] = {
 		{80, 75, 90}, 
 		{60, 70, 85}, 
@@ -10,11 +10,20 @@ int main(){
 	};
 	
 	for(int i = 0; i<4; i++){
-		for(int j = 0; j< 3; j++){
+		for(j = 0; j< 3; j++){
 			sum = sum + array[i][j];
 			
 	}
 	printf("Total for student %d is %d\n", i + 1, sum);
 	sum = 0;
+}
+		
+	for(int i = 0; i<3; i++){
+		for(j = 0; j< 4; j++){
+			subsum = subsum + array[j][i];
+		}
+        printf("Avg of Subject %d is %d\n", i + 1, subsum/4);
+		subsum = 0;
+			
 }
 }
